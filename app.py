29b0ce724823758b4b7c3214914bd011290eefd6
Joshua Christian <joshua3christianx@gmail.com>
2020-10-12 17:23:11 +0100

    renamed main to app

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def hello_semicolon():
     return 'Hello, Semicolon!'
 
 
 @app.route('/ping')
-def hello_world():
+def ping():
     return 'pong'
 
 
